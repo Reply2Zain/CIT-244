93abb6da1081fdada6d9bf407f8c87cbc956a65c
zain.aaban <zain.aaban@AL-L509-S-13.ccac.org>
2019-02-11 14:28:38 -0500

    v1.0.1 - Works with minimal functionality 		- Documentation added

--- PasswordSystem/src/passwordsystem/PasswordSystem.java
+++ PasswordSystem/src/passwordsystem/PasswordSystem.java
@@ -19,8 +19,10 @@ public class PasswordSystem {
    
   
    /**
-    * 
-    * @return 
+    * Welcomes the user with an introductory phrase letting
+    * the user know what the program is and what it is called
+    * @return null
+    * @param null
     */
 public String getWelcomeText(){
        System.out.println("Welcome to the Password System");
@@ -28,12 +30,25 @@ public String getWelcomeText(){
 }
 //creates the "user base size"
    String[] passwords = new String[Userbase];
-   
+
+   /**
+    * This sets the size of the array which holds
+    * the passwords
+    * @param x
+    * @return Userbase
+    */
 public int setUserBase(int x){
     Userbase = x;
     System.out.println("Userbase is " + Userbase);
     return Userbase;
 }
+
+/**
+ * This asks the user to enter a password
+ * and the enters the password into an open
+ * array slot
+ * @return null
+ */
 public String askPassword(){
     Scanner sc = new Scanner(System.in);
     Input = sc.next();
@@ -43,16 +58,17 @@ public String askPassword(){
     return null;
 }
 
+//Will get the pass length, does not work yet
 public int getPassLength(){
     
         return 0;
 }
     
     public static void main(String[] args) {
-        PasswordSystem ap = new PasswordSystem();
-        ap.getWelcomeText();
-        ap.setUserBase(5);
-        ap.askPassword();
+        PasswordSystem ps = new PasswordSystem();
+        ps.getWelcomeText();
+        ps.setUserBase(5);
+        ps.askPassword();
     }
     
 }
