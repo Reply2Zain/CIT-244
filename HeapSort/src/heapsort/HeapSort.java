40a5e174bf3dfd3e815e3a2ea1a566709e9a342f
Reply2Zain <37006843+Reply2Zain@users.noreply.github.com>
2019-04-03 16:33:04 -0400

    Add files via upload

---
+++ HeapSort/src/heapsort/HeapSort.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heapsort;
+    
+    
+/**
+ *
+ * @author Zain.aaban
+ */
+public class HeapSort {
+    int largestNumber;
+    static int[] arr = {10, 4, 7, 2, 1, 9};
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    public static void mergeSort(int [] list){
+        if (list.length > 1)
+            
+    }        
+    
+    public static int [] merge(int [] list1, int[] list2){
+        int cursor1 = 0; 
+        int cursor2 = 0;
+        
+        return list2;
+    }
+    
+}
